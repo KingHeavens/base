aa21ac9143a760abfe50859f95eebec69e8e7edb
J.Heavens <zhjing03@126.com>
2019-08-29 20:14:29 +0800

    change: add some topic

---
+++ src/leetcode/Topic303.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+/**
+ * 功能描述
+ *
+ * @author J.Heavens
+ * @version 1.0
+ * @since create at 2019/8/29
+ **/
+public class Topic303 {
+}
---
+++ src/leetcode/Topic347.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+/**
+ * 功能描述
+ *
+ * @author J.Heavens
+ * @version 1.0
+ * @since create at 2019/8/28
+ **/
+public class Topic347 {
+}
