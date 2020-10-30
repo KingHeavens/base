17bc5d4337f1037a75f5cea4200dea79bd6f5ba2
J.Heavens <zhjing03@126.com>
2020-10-30 21:20:29 +0800

    feature:add heap merge qicik sort

--- src/algorithm/HeapSort.java
+++ src/algorithm/HeapSort.java
@@ -1,22 +1,110 @@
 package algorithm;
 
+import test.HeavensTest;
+import test.TestGenerator;
+
+import java.util.Arrays;
+
 /**
  * 堆排序
+ * 堆结构是一个完全二叉树
+ * 可以用数组表示
+ * 堆的左孩子：left = 2*index + 1
+ * 堆的右孩子: right = 2*index + 2
+ * 堆的父节节点; parent = (index - 1) / 2
  *
  * @author J.Heavens
  * @version 1.0
  * @since create at 2020/10/27
  **/
 public class HeapSort {
+    /**
+     * 思路:将数组构建成大根堆，然后堆顶数据与数组最后一个数交换，
+     * 然后将新的堆大小-1，堆化新的堆，不断循环这个步骤直到堆的大小变为1
+     *
+     */
     public void heapSort(int[] arr, int N) {
-
+        if (arr == null || N < 2) {
+            return;
+        }
+        for (int i = 0; i < N; i++) {//「0 - N」建堆
+            heapInsert(arr, i);
+        }
+        int heapSize = N;
+        while (heapSize > 1) {
+            swap(arr, 0, --heapSize);
+            heapify(arr, 0, heapSize);//交换 0 和 heapSize - 1位置后堆化新的堆
+        }
     }
 
+    /**
+     * 在数组index位置向上形成堆
+     * O(log(N))
+     */
     public void heapInsert(int[] arr, int index) {
-
+        while (arr[index] > arr[(index - 1) / 2]) { //当前节点比自己的父节点大的时候不断比较交换
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
     }
 
+    /**
+     * 将数组index位置堆化，往下形成堆
+     *
+     * @param arr arr
+     * @param index 位置
+     * @param heapSize 堆大小
+     */
     public void heapify(int[] arr, int index, int heapSize) {
+        int left = index * 2 + 1; //找到左节点
+        while (left < heapSize) { //不断查找左子节点直到没有左子节点
+            //选取 左子节点 和 右子节点中最大的一个
+            int largestIndex = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
+            //选取当前节点和孩子节点中最大的一个
+            largestIndex = arr[index] > arr[largestIndex] ? index : largestIndex;
+            //如果当前节点就是最大的，说明堆化已完成
+            if (largestIndex == index) {
+                break;
+            }
+            //当前节点与最大节点交换
+            swap(arr, index, largestIndex);
+            //当前节点指向选取的最大节点的位置
+            index = largestIndex;
+            //找到新节点的左孩子节点
+            left = index * 2 + 1;
+        }
+    }
 
+    private void swap(int[] arr, int i ,int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        HeavensTest<Integer> integerHeavensTest = new HeavensTest<>(Integer.class);
+        integerHeavensTest.input(() -> TestGenerator.generateRandomArray(-10000, 10000, 100));
+        integerHeavensTest.addTestCase(new HeavensTest.ITestCase<Integer>() {
+            @Override
+            public Integer[] test(Integer[] testData) {
+                HeapSort heapSort = new HeapSort();
+                int[] testArray = new int[testData.length];
+                for (int i = 0; i < testArray.length; i++) {
+                    testArray[i] = testData[i];
+                }
+                heapSort.heapSort(testArray, testArray.length);
+                for (int i = 0; i < testArray.length; i++) {
+                    testData[i] = testArray[i];
+                }
+                return testData;
+            }
+
+            @Override
+            public Integer[] expect(Integer[] testData) {
+                Arrays.sort(testData);
+                return testData;
+            }
+        });
+        integerHeavensTest.test(10);
     }
 }
--- src/algorithm/QuickSort.java
+++ src/algorithm/QuickSort.java
@@ -1,5 +1,12 @@
 package algorithm;
 
+import test.HeavensTest;
+import test.Printer;
+import test.TestGenerator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * 快速排序
  *
@@ -17,26 +24,118 @@ public class QuickSort {
      * @param num num
      */
     public void dutchFlagQuestion(int[] arr, int num) {
-        partitionDutchFlag(arr, 0, arr.length, num);
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        partitionDutchFlag(arr, 0, arr.length - 1, num);
     }
 
     private int[] partitionDutchFlag(int[] arr, int L, int R, int num) {
-        return null;
+        int less = L - 1;//初始化小于num区域
+        int more = R + 1;//初始化大于num区域
+        while(L < more) {//当指针位置到达大于区域之前一直与num做比较
+            if (arr[L] < num) {
+                //当前位置的数小于num的时候当前位置与小于位置的后一个位置交换，小于位置+1，当前指针位置+1
+                swap(arr, L++, ++less);
+            } else if (arr[L] > num) {
+                //当亲位置的数大于num的时候当前位置与大于区域的前一个位置交换，大于区域+1，当前指针不变
+                swap(arr, L, --more);
+            } else {
+                //当前位置的数等于num的时候，当前指针+1
+                L++;
+            }
+        }
+        // 将等于区域返回
+        int[] result = new int[2];
+        result[0] = less + 1;
+        result[1] = more - 1;
+        return result;
     }
 
     public void quickSort(int[] arr, int N) {
-
+        if (arr == null || N < 2) {
+            return;
+        }
+        quickSort(arr, 0, N - 1);
     }
 
-    public void quickSort(int[] arr, int L, int R) {
-
+    private void quickSort(int[] arr, int L, int R) {
+        if (R <= L) {
+            return;
+        }
+        int random = (int)(Math.random() * (R - L + 1));
+        swap(arr, L + random, R);
+        int[] partitionResult = partition(arr, L, R);
+        quickSort(arr, L, partitionResult[0] - 1);
+        quickSort(arr, partitionResult[1] + 1, R);
     }
 
-    public int[] partition(int[] arr, int L, int R) {
-        return null;
+
+    private int[] partition(int[] arr, int L, int R) {
+        int less = L - 1;
+        int more = R;
+        while (L < more) {
+            if (arr[L] > arr[R]) {
+                swap(arr, L, --more);
+            } else if (arr[L] < arr[R]) {
+                swap(arr, L++, ++less);
+            } else {
+                L++;
+            }
+        }
+        swap(arr, more, R);
+        int[] result = new int[2];
+        result[0] = less + 1;
+        result[1] = more;
+        return result;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     public static void main(String[] args) {
+        //testDutchFlag();
+        testQuickSort();
+    }
 
+    private static void testQuickSort() {
+        HeavensTest<Integer> integerHeavensTest = new HeavensTest<>(Integer.class);
+        integerHeavensTest.input(() -> TestGenerator.generateRandomArray(-10000, 10000, 10));
+        integerHeavensTest.addTestCase(new HeavensTest.ITestCase<Integer>() {
+            @Override
+            public Integer[] test(Integer[] testData) {
+                QuickSort quickSort = new QuickSort();
+                int[] testArray = new int[testData.length];
+                for (int i = 0; i < testArray.length; i++) {
+                    testArray[i] = testData[i];
+                }
+                quickSort.quickSort(testArray, testArray.length);
+                for (int i = 0; i < testArray.length; i++) {
+                    testData[i] = testArray[i];
+                }
+                return testData;
+            }
+
+            @Override
+            public Integer[] expect(Integer[] testData) {
+                Arrays.sort(testData);
+                return testData;
+            }
+        });
+        integerHeavensTest.test(10);
+    }
+
+    private static void testDutchFlag() {
+        int[] testData = {9, 8, 2, 10, 11, 22, 33, 11, 11, 12, 13, 18, 12};
+        QuickSort quickSort = new QuickSort();
+        quickSort.dutchFlagQuestion(testData, 18);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : testData) {
+            list.add(num);
+        }
+        Printer.printIterator(list);
     }
 }
