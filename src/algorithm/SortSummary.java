2b8ca920c00c94b5bc203fe5b84aa88643c9951e
J.Heavens <zhjing03@126.com>
2020-11-03 10:29:53 +0800

    add sort summary

---
+++ src/algorithm/SortSummary.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+/**
+ * 排序算法        平均时间复杂度       最好       最坏      额外空间复杂度      稳定性     排序方式
+ *
+ * 选择排序        O(N^2)           O(N^2)     O(N^2)    O(1)             不稳定      内排序
+ * 冒泡排序        O(N^2)           O(N)       O(N^2)    O(1)             稳定        内排序
+ * 插入排序        O(N^2)           O(N)       O(N^2)    O(1)             稳定        内排序
+ * 希尔排序        O(n^1.3)         O(NlogN)   O(n^2)    O(1)             不稳定      内排序
+ * 归并排序        O(NlogN)         O(NlogN)   O(NlogN)  O(N)             稳定        外排序
+ * 快排序          O(NlogN)         O(NlogN)    O(N)      O(logN)          不稳定      内排序
+ * 堆排序          O(NlogN)         O(NlogN)    O(NlogN)  O(1)             不稳定      内排序
+ * 计数排序        O(N + k）        O(N + k)    O(N + k)  O(k)             稳定        外排序
+ * 基数排序        O(N * k）        O(N * k）   O(N * k）   O(N+k)           稳定        外排序
+ * 桶排序          O(N + k)        O(N + k)    O(N^2)    O(N+k)            稳定       外排序
+ *
+ **/
+public class SortSummary {
+}
